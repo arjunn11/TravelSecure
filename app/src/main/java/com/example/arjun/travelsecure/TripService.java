890d422441e465f67bd4a498a25741173ca43b4a
Arjun Narayan <arjunn11@gmail.com>
2015-03-04 08:31:37 -0800

    app almost done, need to fix service stopping/restarting and second timer

--- app/src/main/java/com/example/arjun/travelsecure/TripService.java
+++ app/src/main/java/com/example/arjun/travelsecure/TripService.java
@@ -3,12 +3,16 @@ package com.example.arjun.travelsecure;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
-import android.telephony.SmsManager;
 import android.util.Log;
+
+import java.util.List;
 
 public class TripService extends Service {
 
@@ -16,6 +20,8 @@ public class TripService extends Service {
     private String destination;
     private double timeInterval;
     private String phoneNumber = "4259999612";
+    private double latitude;
+    private double longitude;
 
     public TripService() {
     }
@@ -27,26 +33,46 @@ public class TripService extends Service {
         String time = intent.getStringExtra(CreateTripActivity.time_);
         timeInterval = Double.parseDouble(time)*60*1000;//convert from seconds to milliseconds
         timerFunc();
-        checkInTimer();
         return super.onStartCommand(intent, flags, startId);
     }
 
     public void checkInTimer(){
-        new CountDownTimer(120000, 10000){
+        new CountDownTimer(5000, 1000){
             public void onTick(long millisLeft){
 
             }
             public void onFinish(){
                 //get current GPS Coordinates
-                String messageText = "NAME has not responded to their most recent TravelSecure notification." +
+                Log.i(TAG, "send sms");
+                getGPS();
+                Log.i(TAG, "Latitude: " + latitude + " | Longitude: " + longitude);
+                /*String messageText = "NAME has not responded to their most recent TravelSecure notification." +
                         "Make sure they're safe by checking their location at " + "If it helps, they said they were" +
                         "going to be at " + destination + "." ;//store the message
                 SmsManager sms = SmsManager.getDefault();
-                sms.sendTextMessage(phoneNumber, null, messageText, null, null);
+                sms.sendTextMessage(phoneNumber, null, messageText, null, null);*/
             }
         }.start();
     }
 
+    public void getGPS() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> providers = lm.getProviders(true);
+
+        /* Loop over the array backwards, and if you get an accurate location, then break out the loop*/
+        Location l = null;
+
+        for (int i = providers.size() - 1; i >= 0; i--) {
+            l = lm.getLastKnownLocation(providers.get(i));
+            if (l != null) break;
+        }
+
+        double[] gps = new double[2];
+        if (l != null) {
+            latitude = l.getLatitude();
+            longitude = l.getLongitude();
+        }
+    }
 
     public void timerFunc(){
         new CountDownTimer(5000, 1000){
@@ -57,7 +83,7 @@ public class TripService extends Service {
             public void onFinish(){
                 Log.i(TAG, "timer finished");
                 sendNotification();
-                //start 5 minute timer
+                checkInTimer();
             }
         }.start();
     }
@@ -71,14 +97,19 @@ public class TripService extends Service {
         intervalAlert.setWhen(System.currentTimeMillis());
         intervalAlert.setAutoCancel(true);
 
+
+
         Intent intent = new Intent(this, UserCheckInActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        int requestID = (int) System.currentTimeMillis();
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, requestID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         intervalAlert.setContentIntent(pendingIntent);
 
         NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         nm.notify(234353, intervalAlert.build());
         Log.i(TAG, "notification sent");
-
     }
 
     @Override
